edde53478268272e7b7a0e42cf360a968f0d1d7e
MamzelNyf <you@example.comfanny.lefert@gmail.com>
2019-09-17 23:40:06 +0800

    finish tictactoe

--- src/index.js
+++ src/index.js
@@ -1,12 +1,180 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//class Square extends React.Component {
+   // render() {
+    //  return (
+        //////replace onClick={function() {alert('click');}} by onClick={() => alert('click');} to create a fonction
+     //   <button 
+     //     className="square" 
+          ////////By calling this.setState from an onClick handler in the Square’s render method, we tell React to re-render that Square whenever its <button> is clicked
+          ////////onClick={() => this.setState({value: 'X'})}
+          ////////state is considered to be private to a component that defines it, we cannot update the Board’s state directly from Square.
+          ////////The DOM <button> element’s onClick attribute has a special meaning to React because it is a built-in component.
+      //   onClick={() => this.props.onClick()} //React will call the onClick event handler that is defined in Square’s render() method.
+      //  >
+      //   {this.props.value}
+      //  </button>
+      //);
+    //}
+  //}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  //Instead of defining a class which extends React.Component, we can write a function that takes props as input and returns
+  function Square(props){
+    return(
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+  class Board extends React.Component {
+    // constructor(props){
+    //   super(props);
+    //   //Board’s initial state to contain an array of 9 nulls corresponding to the 9 squares
+    //   this.state = {
+    //     squares: Array(9).fill(null),
+    //     xIsNext: true,
+    //   };
+    // }//delete the constructor to handle history
+    
+    renderSquare(i) {
+      //pass a prop called value to the Square:
+      //return <Square value={i} />;
+
+      //now we modify the Board’s renderSquare method to read from squares array
+      return ( // We split the returned element into multiple lines for readability and added parentheses so that JavaScript doesn’t insert a semicolon after return and break our code.
+        <Square 
+          value={this.props.squares[i]} 
+          //Square call that function when a square is clicked
+          onClick={() => this.props.onClick(i)} // Square calls function this.handleClick(i) when clicked.
+        //In React, it’s conventional to use on[Event] names for props which represent events and handle[Event] for the methods which handle the events.
+        />
+      );
+    }
+  
+    render() {
+      return (
+        <div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    //set up the initial state for the Game component within its constructor:
+    constructor(props) {
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null),
+        }],
+        stepNumber: 0,
+        xIsNext: true,
+      };
+    }
+
+    handleClick(i){
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if(calculateWinner(squares) || squares[i]){
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : '0';
+      this.setState({
+        history: history.concat([{
+        squares: squares
+        }]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+
+    jumpTo(step){
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
+    }
+    render() {
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) =>{
+        const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
+      let status;
+      if(winner){
+        status ='Winner: '+ winner;
+      }else{
+      status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
+      }
+
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board 
+              squares={current.squares}
+              onClick={i=> this.handleClick(i)}
+            />
+
+          </div>
+          <div className="game-info">
+            <div>{ status }</div>
+            <ol>{ moves }</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
