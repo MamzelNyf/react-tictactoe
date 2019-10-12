1021c73063e8be1bac5bc223052f057f5fdf5b0f
MamzelNyf <fanny.lefert@hotmail.fr>
2019-10-12 21:30:37 +0800

    Updates

---
+++ precache-manifest.3f65c90d7ba6e7b995240ee04376c315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1152b31aae53d6a3bfb5e9c9454c7847",
+    "url": "/react-tictactoe/build/index.html"
+  },
+  {
+    "revision": "e672c4d56211db957bbe",
+    "url": "/react-tictactoe/build/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "8f99ca41385b3543272a",
+    "url": "/react-tictactoe/build/static/js/2.92116912.chunk.js"
+  },
+  {
+    "revision": "e672c4d56211db957bbe",
+    "url": "/react-tictactoe/build/static/js/main.e7700161.chunk.js"
+  },
+  {
+    "revision": "6519a252dc979e5b94c6",
+    "url": "/react-tictactoe/build/static/js/runtime~main.1dd13d48.js"
+  }
+]);
