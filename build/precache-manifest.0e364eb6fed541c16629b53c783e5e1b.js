8f7fb083e82d84ba552fd298dff312c6b3e78b88
MamzelNyf <fanny.lefert@hotmail.fr>
2019-10-12 21:07:42 +0800

    build

---
+++ build/precache-manifest.0e364eb6fed541c16629b53c783e5e1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa10af53b04fb3c7677a11041d5aba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1def3090690c2ad0eae",
+    "url": "/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "8f99ca41385b3543272a",
+    "url": "/static/js/2.92116912.chunk.js"
+  },
+  {
+    "revision": "e1def3090690c2ad0eae",
+    "url": "/static/js/main.4b697a1b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
